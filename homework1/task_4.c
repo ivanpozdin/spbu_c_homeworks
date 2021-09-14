b7343777e83425b185b32b2e83a4b5e72ca8d5ea
ivanpozdin <ivanllpozdin@gmail.com>
2021-09-14 15:17:27 +0300

    Homework1 task_4

--- homework1/task_4.c
+++ homework1/task_4.c
@@ -3,28 +3,29 @@
 
 void printPrimeNumsUptoN(int N)
 {
-	char *isNotPrime = calloc(N, sizeof(char));
-	for (int i = 2; i * i < N; i++) {
-		if (!isNotPrime[i]) {
-			for (int j = 2 * i; j < N; j += i) {
-				isNotPrime[j] = 1;
-			}
-		}
-	}
-	printf("Простые числа до %i:\n", N);
-	for (int i = 2; i < N; i++) {
-		if (!isNotPrime[i]) {
-			printf("%i\n", i);
-		}
-	}
-	free(isNotPrime);
+    char* isNotPrime = calloc(N, sizeof(char));
+    for (int i = 2; i * i < N; i++) {
+        if (!isNotPrime[i]) {
+            for (int j = 2 * i; j < N; j += i) {
+                isNotPrime[j] = 1;
+            }
+        }
+    }
+    printf("Простые числа до %i:\n", N);
+    for (int i = 2; i < N; i++) {
+        if (!isNotPrime[i]) {
+            printf("%i\n", i);
+        }
+    }
+    free(isNotPrime);
 }
 
 int main()
 {
-	int N = 0;
-	printf("Введите число, до которого будут выводиться простые числа: ");
-	scanf("%i", &N);
-	printPrimeNumsUptoN(N);
-	return 0;
+    int N = 0;
+    printf("Введите число, до которого будут выводиться простые числа: ");
+    scanf("%i", &N);
+    printPrimeNumsUptoN(N);
+
+    return 0;
 }
