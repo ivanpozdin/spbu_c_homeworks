1194aef3b6d23ff70a4fd42d65ed9edd12a5ba42
ivanpozdin <ivanllpozdin@gmail.com>
2021-09-19 00:16:01 +0300

    Task 1 from homework 1 reformatted with clang-format

--- Homework1/task_1.c
+++ Homework1/task_1.c
@@ -3,26 +3,26 @@
 
 int countQuotient(int dividend, int divisor)
 {
-	int quotient = 0, absDividend = abs(dividend), absDivisor = abs(divisor);
-	while (absDividend >= absDivisor) {
-		absDividend -= absDivisor;
-		++quotient;
-	}
+    int quotient = 0, absDividend = abs(dividend), absDivisor = abs(divisor);
+    while (absDividend >= absDivisor) {
+        absDividend -= absDivisor;
+        ++quotient;
+    }
 
-	if (dividend < 0) {
-		quotient = -quotient - 1;
-	}
-	if (divisor < 0) {
-		quotient = -quotient;
-	}
-	return quotient;
+    if (dividend < 0) {
+        quotient = -quotient - 1;
+    }
+    if (divisor < 0) {
+        quotient = -quotient;
+    }
+    return quotient;
 }
 
 int main()
 {
-	int dividend, divisor;
-	printf("Введите делимое и делитель: ");
-	scanf("%i %i", &dividend, &divisor);
-	printf("Неполное частное равно %i", countQuotient(dividend, divisor));
-	return 0;
+    int dividend, divisor;
+    printf("Введите делимое и делитель: ");
+    scanf("%i %i", &dividend, &divisor);
+    printf("Неполное частное равно %i", countQuotient(dividend, divisor));
+    return 0;
 }
