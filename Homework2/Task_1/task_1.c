6a8d4849f1e981ebec4823afed6a7cbdfc1ceea7
Ivan Pozdin <ivanllpozdin@gmail.com>
2021-10-08 22:05:28 +0300

    Now program gets paths as arguments at startup

--- Homework2/Task_1/task_1.c
+++ Homework2/Task_1/task_1.c
@@ -1,17 +1,16 @@
 #include "linked_map.h"
 
-int main()
+int main(int argc, char* argv[], char* envp[])
 {
     LinkedMap* map = makeNewLinkedMap();
     char nameOfInputFile[150] = "";
-    printf("Enter the name of the file whose contents you want to analyze:\n");
     scanf("%s", nameOfInputFile);
     FILE* inputFile = fopen(nameOfInputFile, "r");
 
     char nameOfOutputFile[150] = "";
-    printf("Enter the name of the file in which you want to record the result of the frequency analysis:\n");
     scanf("%s", nameOfOutputFile);
     FILE* outputFile = fopen(nameOfOutputFile, "w");
+
     if ((bool)inputFile ^ (bool)outputFile) {
         if (inputFile)
             fclose(inputFile);
@@ -25,23 +24,9 @@ int main()
     while (fscanf(inputFile, "%s", word) != EOF) {
         put(map, word, get(map, word, 0) + 1);
     }
-    int variant = 0;
-    printf("If you want to see the entire list of pairs of the form - word, occurrence - enter 0\n");
-    printf("If you want to see the occurrence of specific words,\nthen enter 1, and then the number of different words, then enter these words\n");
-    scanf("%i", &variant);
-    if (variant == 0)
-        printAllKeysAndValues(outputFile, map, ',');
-    else if (variant == 1) {
-        int amountOfWords = 0;
-        scanf("%i", &amountOfWords);
-        char key[128];
-        for (int i = 0; i < amountOfWords; i++) {
-            scanf("%s", key);
-            fprintf(outputFile, "%s %i\n", key, get(map, key, 0));
-        }
-    } else {
-        printf("You entered wrong variant");
-    }
+
+    printAllKeysAndValues(outputFile, map, ',');
+
     deleteMap(map);
     fclose(inputFile);
     fclose(outputFile);
