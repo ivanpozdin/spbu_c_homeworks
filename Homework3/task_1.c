bf3312909fb18dfaea6c0d599efaa64b3a0fe1e5
Ivan Pozdin <ivanllpozdin@gmail.com>
2021-10-15 11:27:16 +0300

    + function addTwoBinaryNumbers

--- Homework3/task_1.c
+++ Homework3/task_1.c
@@ -1,29 +1,57 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-void printBinaryNumber(int number)
+void printBinaryNumber(bool* binaryNumber)
 {
-    printf("%i", number < 0);
-    if (number < 0)
-        for (int i = 31; i >= 0; i--)
-            printf("%i", !((bool)(1<<i & number)));
-    else
-        for (int i = 31; i >= 0; i--)
-            printf("%i", (bool)(1<<i & number));
+    for (int i = 31; i >= 0; i--)
+        printf("%d", binaryNumber[i]);
+}
+void convertFromDecimalToBinary(bool* binaryNumber, int decimalNumber)
+{
+    for (int i = 31; i >= 0; i--)
+        binaryNumber[i] = (bool)(1 << i & decimalNumber);
+}
+
+void addTwoBinaryNumbers(const bool* number1, const bool* number2, bool* result)
+{
+    bool inMind = 0;
+    for (int i = 0; i < 32; i++) {
+        if (inMind) {
+            if (number1[i] && number2[i])
+                result[i] = 1;
+            else if (number1[i] ^ number2[i])
+                result[i] = 0;
+            else {
+                result[i] = 1;
+                inMind = 0;
+            }
+        } else {
+            if (number1[i] && number2[i]) {
+                result[i] = 0;
+                inMind = 1;
+            } else if (number1[i] ^ number2[i])
+                result[i] = 1;
+            else
+                result[i] = 0;
+        }
+    }
 }
 
 int main()
 {
     int a, b;
-    printf("Enter two numbers: ");
+    printf("Enter two integer numbers: ");
     scanf("%i %i", &a, &b);
-    printf("First number in binary form: ");
-    printBinaryNumber(a);
-    printf("\nSecond number in binary form: ");
-    printBinaryNumber(b);
-    printf("\nSum of the numbers in decimal form: %i, in binary form: ", a + b);
-    printBinaryNumber(a + b);
+    bool binaryA[32], binaryB[32], binarySum[32];
+    convertFromDecimalToBinary(binaryA, a);
+    convertFromDecimalToBinary(binaryB, b);
+    printf("First number in binary form:\n");
+    printBinaryNumber(binaryA);
+    printf("\nSecond number in binary form:\n");
+    printBinaryNumber(binaryB);
+    addTwoBinaryNumbers(binaryA, binaryB, binarySum);
+    printf("\nSum of the numbers in decimal form: %i, in binary form:\n", a + b);
+    printBinaryNumber(binarySum);
 
     return 0;
 }
