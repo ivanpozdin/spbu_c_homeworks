24e14033f7cf698a248010d40d8465ab8ae6abb8
Ivan Pozdin <ivanllpozdin@gmail.com>
2021-10-31 18:35:35 +0300

    Added suggested changes

--- Homework3/task_1.c
+++ Homework3/task_1.c
@@ -3,48 +3,45 @@
 
 void printBinaryNumber(bool* binaryNumber)
 {
-    for (int i = 31; i >= 0; i--)
+    for (int i = sizeof(int) * 8 - 1; i >= 0; i--)
         printf("%d", binaryNumber[i]);
 }
-void convertFromDecimalToBinary(bool* binaryNumber, int decimalNumber)
+
+void convertFromIntToBinary(bool* binaryNumber, int decimalNumber)
 {
-    for (int i = 31; i >= 0; i--)
+    for (int i = sizeof(int) * 8 - 1; i >= 0; i--)
         binaryNumber[i] = (bool)(1 << i & decimalNumber);
 }
 
 void addTwoBinaryNumbers(const bool* number1, const bool* number2, bool* result)
 {
     bool inMind = 0;
-    for (int i = 0; i < 32; i++) {
-        if (inMind) {
-            if (number1[i] && number2[i])
-                result[i] = 1;
-            else if (number1[i] ^ number2[i])
-                result[i] = 0;
-            else {
-                result[i] = 1;
-                inMind = 0;
-            }
-        } else {
-            if (number1[i] && number2[i]) {
-                result[i] = 0;
-                inMind = 1;
-            } else if (number1[i] ^ number2[i])
-                result[i] = 1;
-            else
-                result[i] = 0;
-        }
+    for (int i = 0; i < sizeof(int) * 8; i++) {
+        int sum = number1[i] + number2[i] + inMind;
+        result[i] = sum % 2;
+        inMind = sum / 2;
     }
+}
+
+void fillArrayWithZeroes(bool* array, int amountOfElements)
+{
+    for (int i = 0; i < amountOfElements; i++)
+        array[i] = 0;
 }
 
 int main()
 {
-    int a, b;
+    int a = 0, b = 0;
     printf("Enter two integer numbers: ");
     scanf("%i %i", &a, &b);
-    bool binaryA[32], binaryB[32], binarySum[32];
-    convertFromDecimalToBinary(binaryA, a);
-    convertFromDecimalToBinary(binaryB, b);
+
+    bool binaryA[sizeof(int) * 8], binaryB[sizeof(int) * 8], binarySum[sizeof(int) * 8];
+    fillArrayWithZeroes(binaryA, sizeof(int) * 8);
+    fillArrayWithZeroes(binaryB, sizeof(int) * 8);
+    fillArrayWithZeroes(binarySum, sizeof(int) * 8);
+
+    convertFromIntToBinary(binaryA, a);
+    convertFromIntToBinary(binaryB, b);
     printf("First number in binary form:\n");
     printBinaryNumber(binaryA);
     printf("\nSecond number in binary form:\n");
