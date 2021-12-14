e169918e89ff913128b2529252f8c811d48b9fbf
Ivan Pozdin <ivanllpozdin@gmail.com>
2021-12-14 14:13:43 +0300

    +вроде работает, но output файл не создает, а принимает

---
+++ Retest1/test1_task2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char nameOfInputFile[150] = "";
+    char nameOfOutputFile[150] = "";
+    printf("Enter the name of the file whose contents you want to analyze:\n");
+    scanf("%s", nameOfInputFile);
+    printf("Enter the name of the file in which you want to record the result of the frequency analysis:\n");
+    scanf("%s", nameOfOutputFile);
+    FILE* inputFile = fopen(nameOfInputFile, "r");
+    FILE* outputFile = fopen(nameOfOutputFile, "w");
+    char c = '0';
+    int statistic[26];
+    for (int i = 0; i < 26; i++)
+        statistic[i] = 0;
+
+    while ((c = getc(inputFile)) != EOF) {
+        if ((int)c > 96 && (int)c < 123)
+            statistic[(int)c - 97]++;
+    }
+    for (int i = 0; i < 26; i++){
+        if (statistic[i] > 0)
+        fprintf(outputFile, "%c: %i\n", (char)(97+i), statistic[i]);
+    }
+    fclose(inputFile);
+    fclose(outputFile);
+
+}
